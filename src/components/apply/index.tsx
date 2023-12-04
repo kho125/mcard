e706efdf65eb3620601d80a446f14322a561fe2d
kho125 <kyeongho94@gmail.com>
2023-12-04 16:43:38 +0900

    카드 신청중 이탈 - 예외처리

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -10,31 +10,45 @@ function Apply({ onSubmit }: { onSubmit: (applyValues: ApplyValues) => void }) {
   const user = useUser()
   const { id } = useParams() as { id: string }
 
-  const [step, setStep] = useState(0)
-  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>({
-    userId: user?.uid,
-    cardId: id
+  const storageKey = `applied-${user?.uid}-${id}`
+
+  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>(() => {
+    const applied = localStorage.getItem(storageKey)
+
+    if (applied == null) {
+      return {
+        userId: user?.uid,
+        cardId: id,
+        step: 0
+      }
+    }
+
+    return JSON.parse(applied)
   })
 
   useEffect(() => {
-    if (step === 3) {
+    if (applyValues.step === 3) {
+      localStorage.removeItem(storageKey)
+
       onSubmit({
         ...applyValues,
         appliedAt: new Date(),
         status: APPLY_STATUS.REDAY,
       } as ApplyValues)
+    } else {
+      console.log('저장', applyValues)
+      localStorage.setItem(storageKey, JSON.stringify(applyValues))
     }
-  }, [applyValues, step, onSubmit])
+  }, [applyValues, onSubmit, storageKey])
 
   const handleTermsChange = (terms: ApplyValues['terms']) => {
     console.log('terms', terms)
 
     setApplyValues((prevValues) => ({
       ...prevValues,
-      terms
+      terms,
+      step: (prevValues.step as number) + 1,
     }))
-    
-    setStep((prevStep) => prevStep + 1)
   }
 
   const handleBasicInfoChange = (
@@ -42,28 +56,26 @@ function Apply({ onSubmit }: { onSubmit: (applyValues: ApplyValues) => void }) {
   ) => {
     setApplyValues((prevValues) => ({
       ...prevValues,
-      ...infoValues
+      ...infoValues,
+      step: (prevValues.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
 
-  const hangleCardInfoChange = (
-    cardInfoValues = Pick<ApplyValues, 'isHipass' | 'isMaster' | 'isRf'>
+  const handleCardInfoChange = (
+    cardInfoValues: Pick<ApplyValues, 'isHipass' | 'isMaster' | 'isRf'>,
   ) => {
     setApplyValues((prevValues) => ({
       ...prevValues,
-      ...cardInfoValues
+      ...cardInfoValues,
+      step: (prevValues.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
 
   return (
     <div>
-      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
-      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
-      {step === 2 ? <CardInfo onNext={hangleCardInfoChange} /> : null}
+      {applyValues.step === 0 ? <Terms onNext={handleTermsChange} /> : null}
+      {applyValues.step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
+      {applyValues.step === 2 ? <CardInfo onNext={handleCardInfoChange} /> : null}
     </div>
   )
 }
--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -7,14 +7,45 @@ import { updateApplyCard } from '@/remote/apply'
 import { APPLY_STATUS } from '@/models/apply'
 import useUser from '@/hooks/auth/useUser'
 import { useNavigate, useParams } from 'react-router-dom'
+import useAppliedCard from '@/components/apply/hooks/useAppliendCard'
+import { useAlertContext } from '@/contexts/AlertContext'
+import FillPageLoader from '@shared/FullPageLoader'
 
 function ApplyPage() {
   const navigate = useNavigate()
+  const { open } = useAlertContext()
 
   const [readyToPoll, setReadyToPoll] = useState(false) // 폴링 할 준비가 되었냐~
 
   const user = useUser()
   const { id } = useParams() as { id: string }
+
+  const { data } = useAppliedCard({
+    userId: user?.uid as string,
+    cardId: id,
+    options: {
+      onSuccess: (applied) => {
+        if (applied == null) {
+          return
+        }
+
+        if (applied.status === APPLY_STATUS.COMPLETE) {
+          open({
+            title: '이미 발급이 완료된 카드입니다',
+            onButtonClick: () => {
+              window.history.back()
+            }
+          })
+
+          return
+        }
+
+        setReadyToPoll(true)
+      },
+      onError: () => {},
+      suspense: true
+    }
+  })
 
   usePollApplyStatus({
     onSuccess: async () => {
@@ -56,8 +87,12 @@ function ApplyPage() {
     }
   })
 
+  if (data != null && data.status === APPLY_STATUS.COMPLETE) {
+    return null // 이미 발급이 된 카드라면 Alert이 나오면서 빈 화면을 나타낸다
+  }
+
   if (readyToPoll || 카드를신청중인가) {
-    return <div>Lodding...</div>
+    return <FillPageLoader message="카드를 신청중입니다" />
   }
 
   return <Apply onSubmit={mutate} />
